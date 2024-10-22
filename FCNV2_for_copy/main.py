029b49643c0fd32b9df65671445fc900667b6ae7
yungyun <carol8351119@gmail.com>
2024-10-23 00:37:27 +0800

    change for run gpu

--- FCNV2_for_copy/main.py
+++ FCNV2_for_copy/main.py
@@ -1,13 +1,14 @@
 import argparse
 from modules.inference_weather import FCN2_weather
 
-def main(input_data, output_folder, fore_hr):
-    FCN2_weather(input_data, output_folder, fore_hr)
+def main(input_data, output_folder, fore_hr, device='cpu'):
+    FCN2_weather(input_data, output_folder, fore_hr, device='cpu')
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_data", required=True, help="input_data_file (.nc)")
     parser.add_argument("--output_folder", required=True, help="output_folder (folder)")
     parser.add_argument("--fore_hr", help="how many hours", default=240)
+    parser.add_argument("--device", help="cpu or cuda", default='cpu')
     args = parser.parse_args()
-    main(args.input_data, args.output_folder, args.fore_hr) 
+    main(args.input_data, args.output_folder, args.fore_hr, device=args.device) 
